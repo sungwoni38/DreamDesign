1919a22c7c797972c17812af4f3aab51b190a8f8
LG <hsw522hsw1221@gmail.com>
2022-05-31 01:42:42 +0900

    commit2

--- app/src/main/java/com/example/dreamdesign/MainActivity.java
+++ app/src/main/java/com/example/dreamdesign/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dreamdesign;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -11,6 +12,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,57 +27,134 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     ImageView bottom_like, bottom_search , bottom_card, bottom_more;
-    ImageView img_notification;
-
+    Toolbar my_toolbar;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
+        Toolbar my_toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(my_toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         bottom_like = findViewById(R.id.bottom_like);
-        bottom_like.setOnClickListener(this);
+        bottom_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent01 = new Intent(MainActivity.this, LikeActivity.class);
+                startActivity(intent01);
+            }
+        });
 
         bottom_search = findViewById(R.id.bottom_search);
-        bottom_search.setOnClickListener(this);
+        bottom_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent02 = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent02);
+            }
+        });
 
         bottom_card = findViewById(R.id.bottom_card);
-        bottom_card.setOnClickListener(this);
+        bottom_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent03 = new Intent(MainActivity.this, CardActivity.class);
+                startActivity(intent03);
+            }
+        });
 
         bottom_more = findViewById(R.id.bottom_more);
-        bottom_more.setOnClickListener(this);
+        bottom_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent04 = new Intent(MainActivity.this, MoreActivity.class);
+                startActivity(intent04);
+            }
+        });
 
-        img_notification = findViewById(R.id.img_notification);
-        img_notification.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_noti:
+                Intent intent = new Intent(MainActivity.this, NotiActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
     @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.bottom_like) {
-            Intent intent01 = new Intent(MainActivity.this, LikeActivity.class);
-            startActivity(intent01);
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        //37.557667, 126.926546 홍대입구역
+        LatLng latLng = new LatLng(37.557667, 126.926546);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("홍대입구역");
+        googleMap.addMarker(markerOptions);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            checkLocationPermissionWithRationale();
         }
-        if(view.getId() == R.id.bottom_search) {
-            Intent intent02 = new Intent(MainActivity.this, SearchActivity.class);
-            startActivity(intent02);
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private void checkLocationPermissionWithRationale() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("위치정보")
+                        .setMessage("이 앱을 사용하기 위해서는 위치정보에 접근이 필요합니다. 위치정보 접근을 허용하여 주세요.")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        }).create().show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+            }
         }
-        if(view.getId() == R.id.bottom_card) {
-            Intent intent03 = new Intent(MainActivity.this, CardActivity.class);
-            startActivity(intent03);
-        }
-        if(view.getId() == R.id.bottom_more) {
-            Intent intent04 = new Intent(MainActivity.this, MoreActivity.class);
-            startActivity(intent04);
-        }
-        if(view.getId() == R.id.img_notification) {
-            Intent intent05 = new Intent(MainActivity.this, NotiActivity.class);
-            startActivity(intent05);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        googleMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
         }
     }
 }
